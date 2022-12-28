11d3ea71a55ca41ada5cdfd0f2b9fda3fdf6ad03
Siva Kumar Tadiboina <boinasiva@gmail.com>
2022-12-28 13:45:10 +0800

    my first commit

---
+++ resource.tf
@@ -0,0 +1,17 @@
+resource "azurerm_resource_group" "rg-existing" {
+  name = "rg-terraform-lab-2"
+  location = "East US"
+  tags = {
+    env = "prod"
+  }
+}
+
+resource "azurerm_virtual_network" "vn-existing" {
+  name                = "vn-terraform-lab-2"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg-existing.location
+  resource_group_name = azurerm_resource_group.rg-existing.name
+  tags = {
+    env = "Prod"
+  }
+}
